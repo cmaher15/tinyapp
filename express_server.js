44ffb051f5757e8a1342ce39f2cc2de099a857a3
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-16 18:04:26 -0700

    added second route and template (urls_show.eje.js) git status

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const req = require("express/lib/request");
 const app = express();
 const PORT = 8080;
 
@@ -18,11 +19,16 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (request, response) => {
+  const templateVars = { shortURL: request.params.shortURL, longURL: urlDatabase[request.params.shortURL] };
+  response.render("urls_show", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n")
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
