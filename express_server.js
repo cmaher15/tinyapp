7b6557d33fd54ea1c393ab188844dd7aba9a8414
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-16 20:07:22 -0700

    updated express server to save new short/long url key value pairs to the urlDatabase and to redirect to /urls/INSERTNEWSHORTURLHERE when a POST request is received

--- express_server.js
+++ express_server.js
@@ -34,8 +34,9 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("OK");
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
