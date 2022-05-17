40f655fe7efd37cff3f593aa420bd95fe116c713
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-17 16:50:59 -0700

    added route to edit/update long url with the user's edited url

--- express_server.js
+++ express_server.js
@@ -25,15 +25,18 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//MAIN USER PAGE WHICH STORES THE URL DATABASE
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//PAGE FOR USER TO ADD A NEW URL 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//CODE WHICH LETS USER ADD NEW URL AND GENERATES A RANDOM CODE FOR THAT URL. REDIRECTS THEM TO /SHORTURL PAGE ONCE LINK IS COMPLETE
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
@@ -45,13 +48,30 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+//REDIRECTS USER TO THE ACTUAL WEBSITE REPRESENTED BY SHORT URL
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+//DELETES A LINK
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
+});
+
+//DIRECTS USER TO "EDIT" PAGE FOR THAT LINK WHEN EDIT BUTTON IS CLICKED.
+app.get("/urls/:shortURL/", (req, res) => {
+  res.redirect(`/urls/${req.params.shortURL}`);
+});
+
+//ALLOWS A USER TO EDIT A LONG URL 
+app.post("/urls/:shortURL/", (req, res) => {
+  console.log(req.body);
+  const shortURL = req.params.shortURL;
+  const newLongURL = req.body.longURL;
+  urlDatabase[shortURL] = newLongURL;
+  console.log(urlDatabase);
   res.redirect('/urls');
 });
 
