c2710852665487bb5c012ef40dc7b2b240fd6ee6
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-17 19:14:51 -0700

    Added a login route with a username res.cookie and a redirect to main urls page

--- express_server.js
+++ express_server.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const req = require("express/lib/request");
 const app = express();
+const cookieParser = require('cookie-parser');
 const PORT = 8080;
-
-app.set("view engine", "ejs");
-
-function generateRandomString(url) {
-  const result = Math.random().toString(36).substring(2, 8);
-  return (result);
-}
-
+const bodyParser = require("body-parser");
+const res = require("express/lib/response");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xk": "http://www.google.ca"
 };
 
-const bodyParser = require("body-parser");
-const res = require("express/lib/response");
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+function generateRandomString(url) {
+  const result = Math.random().toString(36).substring(2, 8);
+  return (result);
+}
 
 
 app.get("/", (req, res) => {
@@ -67,12 +68,19 @@ app.get("/urls/:shortURL/", (req, res) => {
 
 //ALLOWS A USER TO EDIT A LONG URL 
 app.post("/urls/:shortURL/", (req, res) => {
-  console.log(req.body);
   const shortURL = req.params.shortURL;
   const newLongURL = req.body.longURL;
   urlDatabase[shortURL] = newLongURL;
   console.log(urlDatabase);
   res.redirect('/urls');
+});
+
+//LOGIN FOR USER
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  console.log(username);
+  res.cookie("username", username);
+  res.redirect("/urls");
 });
 
 app.get("/urls.json", (req, res) => {
