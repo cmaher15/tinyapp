a7571b226238c3637f953dfa9d708ec85ce4953c
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-18 21:33:28 -0700

    In the throws of a LOT of bugs due to reformatting of url Database. Fixing bits at a time.

--- express_server.js
+++ express_server.js
@@ -13,10 +13,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //DATABASES 
 
+// const urlDatabase = {
+//   'b2xVn2': 'http://www.lighthouselabs.ca',
+//   '9sm5xk': 'http://www.google.ca'
+// };
+
 const urlDatabase = {
-  'b2xVn2': 'http://www.lighthouselabs.ca',
-  '9sm5xk': 'http://www.google.ca'
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW"
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW"
+  }
 };
+
 
 const users = {
   'userRandomID': {
@@ -43,13 +55,23 @@ function generateRandomString(url) {
   return (result);
 }
 
+// function findTheURL(urlDatabase) {
+//   for (let ids in urlDatabase) {
+//    let shortURL = urlDatabase[ids];
+//    console.log('shortURL:', shortURL);
+//    let longURL = urlDatabase[shortURL].longURL;
+//    console.log('longURL', longURL);
+//    let userID = urlDatabase[ids].userID;
+//    console.log('userID:', userID)
+//   }
+// }
 
 //ROUTES
 
 //MAIN USER PAGE WHICH STORES THE URL DATABASE
 app.get('/urls', (req, res) => {
   const userID = req.cookies['user_ID'];
-  console.log('userID:', userID);
+  // console.log('userID:', userID);
   const user = users[userID];
   if (user) {
     const templateVars = { user, urls: urlDatabase };
@@ -80,7 +102,7 @@ app.post('/urls', (req, res) => {
   if (user) {
     res.redirect(`/urls/${shortURL}`);
   }
-  res.redirect('/403');
+  res.status(403).send("Error 403 - Forbidden");
 
 });
 
@@ -88,21 +110,22 @@ app.post('/urls', (req, res) => {
 app.get('/urls/:shortURL', (req, res) => {
   const userID = req.cookies['user_ID'];
   const user = users[userID];
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user };
   if (user) {
     res.render('urls_show', templateVars);
-  } res.redirect('/403');
+  } res.status(403).send("Error 403 - Forbidden");
 });
 
 //REDIRECTS USER TO THE ACTUAL WEBSITE REPRESENTED BY SHORT URL
 app.get('/u/:shortURL', (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL].longURL;
+  console.log(longURL)
   res.redirect(longURL);
 });
 
 //DELETES A LINK
 app.post('/urls/:shortURL/delete', (req, res) => {
-  delete urlDatabase[req.params.shortURL];
+  delete urlDatabase[req.params.shortURL]
   res.redirect('/urls');
 });
 
@@ -115,8 +138,8 @@ app.get('/urls/:shortURL/', (req, res) => {
 app.post('/urls/:shortURL/', (req, res) => {
   const shortURL = req.params.shortURL;
   const newLongURL = req.body.longURL;
-  urlDatabase[shortURL] = newLongURL;
-  console.log(urlDatabase);
+  urlDatabase[shortURL].longURL = newLongURL;
+  // console.log(urlDatabase);
   res.redirect('/urls');
 });
 
@@ -131,21 +154,21 @@ app.get('/login', (req, res) => {
 //LOGIN FORM FOR USER
 app.post("/login", (req, res) => {
   const loginEmail = req.body.username;
-  console.log('login email', loginEmail);
+  // console.log('login email', loginEmail);
   const loginPassword = req.body.password;
-  console.log('login password', loginPassword);
+  // console.log('login password', loginPassword);
   for (const id in users) {
     const user = users[id];
-    console.log('user email', user.email);
-    console.log('user password', user.password);
+    // console.log('user email', user.email);
+    // console.log('user password', user.password);
     if (user.email === loginEmail && user.password === loginPassword) {
-      console.log('user object:', user);
+      // console.log('user object:', user);
       res.cookie('user_ID', user.id);
       return res.redirect('/urls');
 
     }
   }
-  return res.redirect('/403');
+  return res.status(403).send("Error 403 - Forbidden");
 });
 
 
@@ -173,9 +196,9 @@ app.post('/register', (req, res) => {
     // console.log('id', id);
     // console.log('user.email:', user.email);
     if (user.email === req.body.username) {
-      return res.redirect('/error/404');
+      return res.status(404).send("Error 404 - Not Found");
     } else if ((req.body.username === "") || (req.body.password === "")) {
-      res.redirect('/error/404');
+      res.status(404).send("Error 404 - Not Found");
     }
   }
   const userID = generateRandomString();
@@ -185,22 +208,22 @@ app.post('/register', (req, res) => {
   return res.redirect('/urls');
 });
 
-//403 ERROR ROUTE
-app.get('/403', (req, res) => {
-  const userID = req.cookies['user_ID'];
-  const user = users[userID];
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user };
-  res.render('url_forbidden', templateVars);
-});
+// //403 ERROR ROUTE
+// app.get('/403', (req, res) => {
+//   const userID = req.cookies['user_ID'];
+//   const user = users[userID];
+//   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user };
+//   res.render('url_forbidden', templateVars);
+// });
 
 
-//404 ERROR ROUTE
-app.get('/error/404', (req, res) => {
-  const userID = req.cookies['user_ID'];
-  const user = users[userID];
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user };
-  res.render('url_error', templateVars);
-});
+// //404 ERROR ROUTE
+// app.get('/error/404', (req, res) => {
+//   const userID = req.cookies['user_ID'];
+//   const user = users[userID];
+//   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user };
+//   res.render('url_error', templateVars);
+// });
 
 
 
