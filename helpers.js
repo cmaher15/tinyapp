038428a17c9958e60b079e1685d3b55284d0eaad
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-19 19:38:23 -0700

    created helpers file to hold reuseable functions

---
+++ helpers.js
@@ -0,0 +1,11 @@
+const getUserByEmail = (email, users) => {
+  for (const id in users) {
+    const user = users[id];
+    if (user.email === email) {
+      return user;
+    }
+  }
+  return null;
+};
+
+module.exports = getUserByEmail;
