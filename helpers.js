d607212f30c2b886f20e35de8b44d9612b7bb029
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-19 20:58:54 -0700

    moved other functions into helper file along with databases required for them

--- helpers.js
+++ helpers.js
@@ -1,3 +1,34 @@
+//DATABASES 
+
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW"
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW"
+  }
+};
+
+const users = {
+  'userRandomID': {
+    id: 'userRandomID',
+    email: 'user@example.com',
+    password: 'raccon-up-a-tree',
+    hashedPassword: 'vrymltknehltm'
+  },
+  'user2RandomID': {
+    id: 'user2RandomID',
+    email: 'user2@example.com',
+    password: 'dishwasher-funk',
+    hashedPassword: '#C5jl36hvl4b6'
+  }
+};
+
+//The above databases are required in this file, as the functions below call on them specifically. 
+
+///TO CHECK THE USERS BY THEIR EMAILS IN THE DATABASE///
 const getUserByEmail = (email, users) => {
   for (const id in users) {
     const user = users[id];
@@ -8,26 +39,30 @@ const getUserByEmail = (email, users) => {
   return undefined;
 };
 
-
-// const generateRandomString = function() {
-//   const result = Math.random().toString(36).substring(2, 8);
-//   return (result);
-// };
-
-
-
-// const urlsForUser = function(loggedInUserID) {
-//   const userURLs = {};
-//   for (let shortURL in urlDatabase) {
-//     if (loggedInUserID === urlDatabase[shortURL].userID) {
-//       userURLs[shortURL] = urlDatabase[shortURL];
-//     }
-//   }
-//   return userURLs;
-// };
+///TO GENERATE A RANDOM STRING TO USE FOR USER ID AND SHORT URLS///
+const generateRandomString = () => {
+  const result = Math.random().toString(36).substring(2, 8);
+  return (result);
+};
 
 
+///TO FILTER THE URLS PAGE BASED ON THE URLS THAT USER HAS MADE///
+const urlsForUser = (loggedInUserID) => {
+  const userURLs = {};
+  for (let shortURL in urlDatabase) {
+    if (loggedInUserID === urlDatabase[shortURL].userID) {
+      userURLs[shortURL] = urlDatabase[shortURL];
+    }
+  }
+  return userURLs;
+};
 
 
-module.exports = getUserByEmail
+
+
+module.exports =  { getUserByEmail,
+generateRandomString,
+urlsForUser 
+};
+ 
 
