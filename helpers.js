e034dd37c1935dfce22e342dbbb1096c88fd748f
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-19 20:28:11 -0700

    tried to move two other functions to helper.js but encountered a lot of bugs. Moved back for now.

--- helpers.js
+++ helpers.js
@@ -5,7 +5,29 @@ const getUserByEmail = (email, users) => {
       return user;
     }
   }
-  return null;
+  return undefined;
 };
 
-module.exports = getUserByEmail;
+
+// const generateRandomString = function() {
+//   const result = Math.random().toString(36).substring(2, 8);
+//   return (result);
+// };
+
+
+
+// const urlsForUser = function(loggedInUserID) {
+//   const userURLs = {};
+//   for (let shortURL in urlDatabase) {
+//     if (loggedInUserID === urlDatabase[shortURL].userID) {
+//       userURLs[shortURL] = urlDatabase[shortURL];
+//     }
+//   }
+//   return userURLs;
+// };
+
+
+
+
+module.exports = getUserByEmail
+
